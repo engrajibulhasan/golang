5752d007691e926d8cf74a6f538c65e951b442e2
Rajibul <rajibul.hasan@yaana.com>
2025-02-10 12:23:05 +0600

    Array Slice class

--- array/array_slice.go
+++ array/array_slice.go
@@ -1,9 +1,5 @@
 package testslice
 
-func Sum(numbers []int) int {
-    sum := 0
-    for _, number := range numbers {
-        sum += number
-    }
-    return sum
+func Sum(numbers [5]int) int {
+    return 0
 }
